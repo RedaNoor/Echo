25d7a58c857bf0bf64c4f640fd3417400eb15038
Rida Noor <61821929+RedaNoor@users.noreply.github.com>
2024-08-15 00:36:56 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 import streamlit as st
-import google.generativeai as genai  # Assuming gemini_ai is the library for Gemini AI
-
+import google.generativeai as genai 
 # Adding title
 st.title('Chatbot')
 
 # Set up your Gemini AI API key
 genai.configure(api_key = st.secrets["Api_key"])
-
-#genai.configure(api_key='AIzaSyBw902ueMsy1kqZiZHPWQ4Ir85JmF4z9KY')
 
 # Define generation configuration
 generation_config = {
